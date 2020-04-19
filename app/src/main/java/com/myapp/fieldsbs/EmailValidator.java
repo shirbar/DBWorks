0b33f25cd4877830c8b2e44aabcaaa1092285538
HenDahan <45328465+HenDahan@users.noreply.github.com>
2020-04-19 16:41:27 +0300

    Email validator
    Email validator  code

---
+++ app/src/main/java/com/myapp/fieldsbs/EmailValidator.java
@@ -0,0 +1,45 @@
+package com.myapp.fieldsbs;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import java.util.regex.Pattern;
+
+
+public class EmailValidator implements TextWatcher {
+    /**
+     * Email validation pattern.
+     */
+    public static final Pattern EMAIL_PATTERN = Pattern.compile(
+            "[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +
+                    "\\@" +
+                    "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
+                    "(" +
+                    "\\." +
+                    "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
+                    ")+"
+    );
+    private boolean mIsValid = false;
+    public boolean isValid() {
+        return mIsValid;
+    }
+
+    public static boolean isValidEmail(CharSequence email) {
+        return email != null && EMAIL_PATTERN.matcher(email).matches();
+    }
+
+    @Override
+    final public void afterTextChanged(Editable editableText) {
+        mIsValid = isValidEmail(editableText);
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+}
