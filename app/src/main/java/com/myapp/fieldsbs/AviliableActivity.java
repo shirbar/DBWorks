9a352c95b4ec3c6435b5f131efc52a8785cb5984
Shir Bar <shirbar1066@gmail.com>
2020-04-28 18:26:14 +0300

    Add user dashboard with the options

---
+++ app/src/main/java/com/myapp/fieldsbs/AviliableActivity.java
@@ -0,0 +1,14 @@
+package com.myapp.fieldsbs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AviliableActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aviliable);
+    }
+}
--- app/src/main/java/com/myapp/fieldsbs/MainActivity.java
+++ app/src/main/java/com/myapp/fieldsbs/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -20,68 +21,50 @@ import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView mainTxt;
-    FirebaseUser user;
-    private FirebaseAuth fAuth;
-    FirebaseDatabase database;
-    DatabaseReference userRef;
-    static final String USERS = "Users";
-
-
+    Button TurnirsBtn, NerbyBtn, AviliableBtn, logOutBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
-        fAuth = FirebaseAuth.getInstance();
-        user = FirebaseAuth.getInstance().getCurrentUser();
-        if (user == null){
-            startActivity(new Intent(getApplicationContext(), LoginActivity.class));
-        }
+        TurnirsBtn = findViewById(R.id.TurnirsBtn);
+        NerbyBtn = findViewById(R.id.NerbyBtn);
+        AviliableBtn = findViewById(R.id.AviliableBtn);
+        logOutBtn = findViewById(R.id.logoutBtn);
 
-        InitializeFields();
 
-        database = FirebaseDatabase.getInstance();
-        userRef = database.getReference(USERS);
-        userRef.addValueEventListener(new ValueEventListener() {
+        TurnirsBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                String id= fAuth.getUid();
-                for(DataSnapshot ds : dataSnapshot.getChildren()) {
-                    if (Objects.requireNonNull(ds.child("id").getValue()).toString().equals(id)){
-                        mainTxt.setText("Welcome " + Objects.requireNonNull(ds.child("name").getValue()).toString() + "!");
-                    }
-                }
-            }
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
+            public void onClick(View v) {
+                Intent i = new Intent(getBaseContext(),TurnirsActivity.class);
+                startActivity(i);
             }
         });
 
+        NerbyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getBaseContext(),CloseFieldActivity.class);
+                startActivity(i);
+            }
+        });
+
+        AviliableBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getBaseContext(),AviliableActivity.class);
+                startActivity(i);
+            }
+        });
+
+        logOutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getBaseContext(),LoginActivity.class);
+                startActivity(i);
+            }
+        });
 
     }
-
-    private void InitializeFields() {
-        mainTxt = findViewById(R.id.tvMainText);
-    }
-
-
-    public void logout(View view){
-        FirebaseAuth.getInstance().signOut();
-        startActivity(new Intent(getApplicationContext(), LoginActivity.class));
-        finish();
-    }
-
-/*
-    @Override
-    public void onBackPressed() {
-        Intent returnIntent = new Intent();
-        returnIntent.putExtra("hasBackPressed",true);
-        setResult(Activity.RESULT_OK,returnIntent);
-        finish();
-    }
-*/
-
-
 }
