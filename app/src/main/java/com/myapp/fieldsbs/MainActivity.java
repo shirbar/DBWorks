be8a0b9739e08fb3fd6925c50b21046f1784fa95
kfir147 <kfir.rn@gmail.com>
2020-04-28 20:14:07 +0300

    main activity listeners are working.

--- app/src/main/java/com/myapp/fieldsbs/MainActivity.java
+++ app/src/main/java/com/myapp/fieldsbs/MainActivity.java
@@ -1,23 +1,14 @@
 package com.myapp.fieldsbs;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
-import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,7 +17,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
+
 
         TurnirsBtn = findViewById(R.id.TurnirsBtn);
         NerbyBtn = findViewById(R.id.NerbyBtn);
@@ -37,34 +29,40 @@ public class MainActivity extends AppCompatActivity {
         TurnirsBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(getBaseContext(),TurnirsActivity.class);
-                startActivity(i);
+                startActivity(new Intent(getApplicationContext(), TurnirsActivity.class));
             }
         });
 
         NerbyBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(getBaseContext(),CloseFieldActivity.class);
-                startActivity(i);
+                startActivity(new Intent(getApplicationContext(), TurnirsActivity.class));
             }
         });
 
         AviliableBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(getBaseContext(),AviliableActivity.class);
-                startActivity(i);
+                startActivity(new Intent(getApplicationContext(), TurnirsActivity.class));
             }
         });
 
         logOutBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(getBaseContext(),LoginActivity.class);
-                startActivity(i);
+                logout();
             }
         });
 
+
+
     }
+
+    public void logout() {
+
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+        finish();
+    }
+
 }
