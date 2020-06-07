e3d9d6cc4d1b7f8d0784169a6268a837916533f5
kfir147 <kfir.rn@gmail.com>
2020-06-07 13:24:25 +0300

    Added AddressMeasurement of the addresses selected by the users of their neighborhoods.

--- app/src/main/java/com/myapp/fieldsbs/FieldsActivity.java
+++ app/src/main/java/com/myapp/fieldsbs/FieldsActivity.java
@@ -41,7 +41,7 @@ public class FieldsActivity extends AppCompatActivity {
     String key, id, type, date, fieldName, userName, UserId, command, neighborhood;
     TextView dateTxt, fieldTxt, userSelect;
     DatePickerDialog.OnDateSetListener mDateListener;
-    DatabaseReference managementRef, usersRef;
+    DatabaseReference managementRef, usersRef, measurementRef;
     Button backBtn, fullAssignBtn, assignMyselfBtn;
     ListView myList;
     ArrayList<String> showList, hoursList, statusList, typeList, numOfPlayersList, creatorList, usersList;
@@ -56,6 +56,7 @@ public class FieldsActivity extends AppCompatActivity {
 
         managementRef = FirebaseDatabase.getInstance().getReference().child("Management");
         usersRef = FirebaseDatabase.getInstance().getReference().child("Users");
+        measurementRef = FirebaseDatabase.getInstance().getReference().child("AddressMeasurement");
         dateTxt = findViewById(R.id.dateTxt);
         fieldTxt = findViewById(R.id.fieldName);
         myList = findViewById(R.id.listView);
@@ -85,6 +86,8 @@ public class FieldsActivity extends AppCompatActivity {
         command = getIntent().getStringExtra("command");
         neighborhood = getIntent().getStringExtra("neighborhood");
         fieldTxt.setText("שם המגרש: " + fieldName);
+        System.out.println("the 11111 command is: " + command);
+
 
 
         dateTxt.setOnClickListener(new View.OnClickListener() {
@@ -308,6 +311,8 @@ public class FieldsActivity extends AppCompatActivity {
     }
 
     public void addPlayer() {
+        System.out.println("doing add");
+
         managementRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
@@ -315,7 +320,6 @@ public class FieldsActivity extends AppCompatActivity {
                 playersNum++;
                 managementRef.child(key).child(date).child(userSelect.getText().toString()).child("numofPlayers").setValue(String.valueOf(playersNum));
                 managementRef.child(key).child(date).child(userSelect.getText().toString()).child("participantList").child(id).child("name").setValue(userName);
-
                 Assign_To_My_Activities();
             }
             @Override
@@ -325,12 +329,13 @@ public class FieldsActivity extends AppCompatActivity {
     }
 
     public void fullAssign() {
+        System.out.println("doing full");
+
         managementRef.child(key).child(date).child(userSelect.getText().toString()).child("creator").setValue(userName);
         managementRef.child(key).child(date).child(userSelect.getText().toString()).child("status").setValue("תפוס");
         managementRef.child(key).child(date).child(userSelect.getText().toString()).child("numofPlayers").setValue("מלא");
         managementRef.child(key).child(date).child(userSelect.getText().toString()).child("type").setValue(type);
         managementRef.child(key).child(date).child(userSelect.getText().toString()).child("participantList").child(id).child("name").setValue(userName);
-
         Assign_To_My_Activities();
     }
 
@@ -344,6 +349,9 @@ public class FieldsActivity extends AppCompatActivity {
     }
 
     private void Assign_To_My_Activities() {
+        if (command.equals("address"))
+            neighborhoodMeasurement(neighborhood);
+
         HashMap<String, Object> Activity = new HashMap<>();
 
         Activity.put("Field name", fieldName);
@@ -361,6 +369,25 @@ public class FieldsActivity extends AppCompatActivity {
                 } else {
                     Toast.makeText(FieldsActivity.this, "הפעילות לא התווספה בהצלחה- אנא נסה שנית", Toast.LENGTH_SHORT).show();
                 }
+            }
+        });
+    }
+
+    private void neighborhoodMeasurement(final String neigh){
+        measurementRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (!dataSnapshot.hasChild(neigh)){
+                    measurementRef.child(neigh).setValue("1");
+                }
+                else{
+                    String old_value = Objects.requireNonNull(dataSnapshot.child(neigh).getValue()).toString();
+                    String new_value = String.valueOf(Integer.parseInt(old_value) + 1);
+                    measurementRef.child(neigh).setValue(new_value);
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
             }
         });
     }
