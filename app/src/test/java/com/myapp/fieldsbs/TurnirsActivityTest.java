e620a30d77b288f89c553a718f620df35f9dd363
barpg2802 <barpg2802@gmail.com>
2020-06-08 20:01:08 +0300

    TESTS - TURNIRS

---
+++ app/src/test/java/com/myapp/fieldsbs/TurnirsActivityTest.java
@@ -0,0 +1,25 @@
+package com.myapp.fieldsbs;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TurnirsActivityTest {
+
+    @Test
+    public void onCreate() {
+        //assertTrue(TurnirsActivity.XX("Kfir123456"));
+    }
+
+    @Test
+    public void deleteActivity() {
+    }
+
+    @Test
+    public void setView() {
+    }
+
+    @Test
+    public void clearLists() {
+    }
+}
